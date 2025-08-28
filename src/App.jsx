4f8c1e588cacdb0af326288c27e4a2d39c5e204b
carolinamontagner <carolina.jaouiche@sptech.school>
2025-08-28 17:50:24 -0300

    tela configurações

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './login';
+<<<<<<< Updated upstream
+=======
+import Dashboard from './dashboard';
+import Estoque from './estoque';
+import Configuracoes from './configuracoes';
+>>>>>>> Stashed changes
 import './App.css';
 
 function App() {
@@ -11,7 +17,14 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Layout />}>
+<<<<<<< Updated upstream
             <Route index element={<Navigate to="/login" replace />} />
+=======
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="estoque" element={<Estoque />} />
+            <Route path="configuracoes" element={<Configuracoes />} />
+>>>>>>> Stashed changes
           </Route>
         </Routes>
       </div>
