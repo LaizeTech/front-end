0affa8923cb2280f0644ec488ea168671e59ba54
PedroPorfirioV <pedro.vieira@sptech.school>
2025-09-03 18:36:00 -0300

    Atualizando pequeno erro

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Login from './login';
 import Dashboard from './dashboard';
 import Estoque from './estoque';
 import Configuracoes from './configuracoes';
-import Funcionarios from './pages/Funcionarios';
+import Funcionarios from './funcionarios';
+import MetricasMensais from './MetricasMensais';
 import './App.css';
 
 function App() {
@@ -15,11 +16,12 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route index element={<Navigate to="/metricas-mensais" replace />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="estoque" element={<Estoque />} />
             <Route path="configuracoes" element={<Configuracoes />} />
             <Route path="funcionarios" element={<Funcionarios />} />
+            <Route path="metricas-mensais" element={<MetricasMensais />} />
           </Route>
         </Routes>
       </div>
