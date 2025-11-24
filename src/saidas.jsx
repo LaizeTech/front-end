deda656faa059a66d410c9e9cd693b91d29b0c27
PedroPorfirioVi <pedro.vieira@sptech.school>
2025-11-23 23:09:45 -0300

    Ajuste fino no front-end nas telas de dashboard, entrada, saída, login e funcionarios

--- src/saidas.jsx
+++ src/saidas.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ChevronDown, Plus, Download } from 'lucide-react';
 import RegistroSaidaModal from './components/RegistroSaidaModal';
-//import './saidas.css';
+import './saidas.css';
 
 const Saidas = () => {
   const [selectedItems, setSelectedItems] = useState([]);
@@ -379,7 +379,7 @@ const Saidas = () => {
                       </div>
                     )}
                   </th>
-                  <th>Quantidade</th>
+                  <th>Qtd</th>
                   <th>Plataforma</th>
                   <th>Data</th>
                   <th>Status do produto</th>
